ac560cbe56c283cc59714fad3226f0eca532a70f
Takumi Yamaga <takumi.yamaga@riken.jp>
2020-03-16 13:45:47 +0900

    modified run action (output ROOT file name)

--- src/RunAction.cc
+++ src/RunAction.cc
@@ -48,7 +48,7 @@ RunAction::RunAction()
   // Default settings
   analysisManager->SetNtupleMerging(true);
   analysisManager->SetVerboseLevel(1);
-  analysisManager->SetFileName("proton_pol");
+  analysisManager->SetFileName("lambda_pol");
 
   // Creating 1D histograms
   analysisManager // H1-ID = 0
